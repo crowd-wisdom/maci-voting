f1758430fa793b9e99dc9bd9af613fdeea8c0d7f
Lautaro Di Sanza <lautarod@gmail.com>
2024-08-07 19:41:56 -0300

    Update MACI Cli

--- extension-chrome/crowdWisdom.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('mostrar-formulario').addEventListener('click', function() {
-    var formulario = document.getElementById('formulario');
-    if (formulario.style.display === 'none') {
-      formulario.style.display = 'block';
-    } else {
-      formulario.style.display = 'none';
-    }
-  });
-  
---
+++ maci-cli/packages/contracts/ts/index.ts
@@ -0,0 +1,35 @@
+export {
+  deployMockVerifier,
+  deployVkRegistry,
+  deployMaci,
+  deployContract,
+  deployContractWithLinkedLibraries,
+  deploySignupToken,
+  deploySignupTokenGatekeeper,
+  deployConstantInitialVoiceCreditProxy,
+  deployFreeForAllSignUpGatekeeper,
+  deployGitcoinPassportGatekeeper,
+  deploySemaphoreGatekeeper,
+  deployPollFactory,
+  createContractFactory,
+  deployPoseidonContracts,
+  deployVerifier,
+} from "./deploy";
+export { genMaciStateFromContract } from "./genMaciState";
+export { genEmptyBallotRoots } from "./genEmptyBallotRoots";
+export { formatProofForVerifierContract, getDefaultSigner, getDefaultNetwork, getSigners } from "./utils";
+export { EMode } from "./constants";
+export { Deployment } from "../tasks/helpers/Deployment";
+export { ContractStorage } from "../tasks/helpers/ContractStorage";
+export { ProofGenerator } from "../tasks/helpers/ProofGenerator";
+export { Prover } from "../tasks/helpers/Prover";
+export {
+  EContracts,
+  type IGenerateProofsOptions,
+  type IGenerateProofsBatchData,
+  type TallyData,
+} from "../tasks/helpers/types";
+export { linkPoseidonLibraries } from "../tasks/helpers/abi";
+
+export type { IVerifyingKeyStruct, SnarkProof, Groth16Proof, Proof } from "./types";
+export * from "../typechain-types";
